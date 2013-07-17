ec8467ece14dceceb6abc821f24d93d1cece8d02
vagrant <vagrant@vagrant.(none)>
2013-07-17 16:41:44 +0000

    First commit in demo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '75278874368712202044cabbdef12a8f162ca69f48212c7f3e9dd17fe22a5704562f3381fdd524375fcaaac35a6ad1e75802247c3965272dc99dffc92db7b148'
